d24900be020fda7e0fcc6eef9bc41c5a6b2103da
Andreas Karampasis <csd3777@csd.uoc.gr>
2023-11-10 23:24:16 +0200

    passes test check opcode

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::random;
+
 const RAM_SIZE: usize = 4096;
 pub const DISPLAY_WIDTH: usize = 64;
 pub const DISPLAY_HEIGHT: usize = 32;
@@ -192,10 +194,92 @@ impl Chip8 {
                 let nn = (op & 0x00FF) as u8;
                 self.v[x] = self.v[x].wrapping_add(nn);
             }
+            (0x8, _, _, 0x0) => {
+                // Set Vx = Vy
+                let x = digit2;
+                let y = digit3;
+                self.v[x as usize] = self.v[y as usize];
+            }
+            (0x8, _, _, 0x1) => {
+                // Set Vx = Vy
+                let x = digit2;
+                let y = digit3;
+                self.v[x as usize] |= self.v[y as usize];
+            }
+            (0x8, _, _, 0x2) => {
+                // Set Vx = Vy
+                let x = digit2;
+                let y = digit3;
+                self.v[x as usize] &= self.v[y as usize];
+            }
+            (0x8, _, _, 0x3) => {
+                // Set Vx = Vy
+                let x = digit2;
+                let y = digit3;
+                self.v[x as usize] ^= self.v[y as usize];
+            }
+            (0x8, _, _, 0x4) => {
+                // Set Vx = Vx + Vy, set VF = carry.
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, carry) = self.v[x].overflowing_add(self.v[y]);
+                self.v[x] = new_vx;
+                self.v[0xF] = if carry { 1 } else { 0 };
+            }
+            (0x8, _, _, 0x5) => {
+                // Set Vx = Vx - Vy, set VF = carry.
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, borrow) = self.v[x].overflowing_sub(self.v[y]);
+                self.v[x] = new_vx;
+                self.v[0xF] = if borrow { 0 } else { 1 };
+            }
+            (0x8, _, _, 0x6) => {
+                // Set Vx = Vx SHR 1
+                let x = digit2 as usize;
+                let lsb = self.v[x] & 1;
+                self.v[x] >>= 1;
+                self.v[0xF] = lsb;
+            }
+            (0x8, _, _, 0x7) => {
+                // Set Vx = Vx - Vy, set VF = carry.
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, borrow) = self.v[y].overflowing_sub(self.v[x]);
+                self.v[x] = new_vx;
+                self.v[0xF] = if borrow { 0 } else { 1 };
+            }
+            (0x8, _, _, 0xE) => {
+                // Set Vx = Vx SHL 1
+                let x = digit2 as usize;
+                let msb = (self.v[x] >> 7) & 1;
+                self.v[x] <<= 1;
+                self.v[0xF] = msb;
+            }
+            (0x9, _, _, 0x0) => {
+                // Skip the next instruction if Vx = Vy
+                let x = digit2;
+                let y = digit3;
+                if self.v[x as usize] != self.v[y as usize] {
+                    self.pc += 2;
+                }
+            }
             (0xA, _, _, _) => {
                 // Set I = nnn
                 let nnn: u16 = op & 0x0FFF;
                 self.i = nnn;
+            }
+            (0xB, _, _, _) => {
+                // Jump to location nnn + V0.
+                let nnn: u16 = op & 0x0FFF;
+                self.pc = (self.v[0] as u16) + nnn;
+            }
+            (0xC, _, _, _) => {
+                // Set Vx = random byte AND kk.
+                let x = digit2 as usize;
+                let nn = (op & 0x00FF) as u8;
+                let rng: u8 = random();
+                self.v[x] = rng & nn;
             }
             (0xD, _, _, _) => {
                 //  Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
@@ -224,7 +308,99 @@ impl Chip8 {
                 }
                 self.v[0xF] = flipped as u8;
             }
-            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
+            (0xE, _, 0x9, 0xE) => {
+                // Skip next instruction if key with the value of Vx is pressed.
+                let x = digit2 as usize;
+                let vx = self.v[x];
+                if self.keys[vx as usize] {
+                    self.pc += 2;
+                }
+            }
+            (0xE, _, 0xA, 0x1) => {
+                //  Set Vx = delay timer value.
+                let x = digit2 as usize;
+                let vx = self.v[x];
+                if !self.keys[vx as usize] {
+                    self.pc += 2;
+                }
+            }
+            (0xF, _, 0x0, 0x7) => {
+                //  Set Vx = delay timer value.
+                let x = digit2 as usize;
+                self.v[x] = self.dt;
+            }
+            (0xF, _, 0x0, 0xA) => {
+                // Wait for a key press, store the value of the key in Vx.
+                let x = digit2 as usize;
+                let mut pressed = false;
+
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v[x] = i as u8;
+                        pressed = true;
+                        break;
+                    }
+                }
+                if !pressed {
+                    // Wait for input
+                    self.pc -= 2;
+                }
+            }
+            (0xF, _, 0x1, 0x5) => {
+                // Set delay timer = Vx
+                let x = digit2 as usize;
+                self.dt = self.v[x];
+            }
+            (0xF, _, 0x1, 0x8) => {
+                // Set sound timer = Vx
+                let x = digit2 as usize;
+                self.st = self.v[x];
+            }
+            (0xF, _, 0x1, 0xE) => {
+                // Set I = I + Vx.
+                let x = digit2 as usize;
+
+                let value = self.v[x] as u16;
+                self.i = self.i.wrapping_add(value);
+            }
+            (0xF, _, 0x2, 0x9) => {
+                // Set I = location of sprite for digit Vx
+                let x = digit2 as usize;
+                let vx = self.v[x] as u16;
+                self.i = vx * 5;
+            }
+            (0xF, _, 0x3, 0x3) => {
+                // Store BCD representatioon of Vx in memory locations I, I+1, I+2
+                let x = digit2 as usize;
+                let vx = self.v[x] as f32;
+                // The interpreter takes the decimal value of Vx, and places the
+                // hundreds digit in memory at location in I, the tens digit at
+                // location I+1, and the ones digit at location I+2.
+                let hundreds = (vx / 100.0).floor() as u8;
+                let tens = ((vx / 10.0) % 10.0).floor() as u8;
+                let ones = (vx % 10.0) as u8;
+
+                self.ram[self.i as usize] = hundreds;
+                self.ram[(self.i + 1) as usize] = tens;
+                self.ram[(self.i + 2) as usize] = ones;
+            }
+            (0xF, _, 0x5, 0x5) => {
+                // Store registers V0 through Vx in memory starting at location I.
+                let x = digit2 as usize;
+                let i = self.i as usize;
+                for idx in 0..=x {
+                    self.ram[i + idx] = self.v[idx];
+                }
+            }
+            (0xF, _, 0x6, 0x5) => {
+                // Read registers V0 through Vx from memory starting at location I.
+                let x = digit2 as usize;
+                let i = self.i as usize;
+                for idx in 0..=x {
+                    self.v[idx] = self.ram[i + idx];
+                }
+            }
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {:#02X}", op),
         }
     }
 }
